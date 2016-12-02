e888a432da75d33c113ae4285bf9f43452c22c8d
ktsn <ktsn55@gmail.com>
2016-12-02 12:03:55 +0900

    Naive implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,77 @@
 // @flow
 
-export const world: string = 'Hello World!'
+import fs from 'fs'
+import path from 'path'
+import url from 'url'
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('This log is output in development mode only') // eslint-disable-line
+interface Options {
+  srcDir: string;
+  compilers: CompilerOptions[];
+}
+
+interface CompilerOptions {
+  reqExt: string;
+  srcExt: string;
+  compile: (src: any, filename: string) => any;
+}
+
+export function compileMiddleware (options: Options) {
+  const compilers = options.compilers.map(c => {
+    return {
+      reqRE: new RegExp(`\\.${c.reqExt}$`),
+      getSrcPath (pathname: string) {
+        return path.join(
+          options.srcDir,
+          pathname.replace(this.reqRE, '.' + c.srcExt)
+        )
+      },
+      compile: c.compile
+    }
+  })
+
+  return (req: any, res: any, done: Function) => {
+    const pathname = normalizePathname(url.parse(req.url).pathname || '')
+
+    forEachAsync(
+      compilers,
+      (compiler, next) => {
+        if (!compiler.reqRE.test(pathname)) return next()
+
+        const srcPath = compiler.getSrcPath(pathname)
+        if (!fs.existsSync(srcPath)) return next()
+
+        fs.readFile(srcPath, (error, data) => {
+          if (error) {
+            console.error(error)
+            return next(true)
+          }
+
+          res.end(compiler.compile(data, srcPath))
+          next(true)
+        })
+      },
+      done
+    )
+  }
+}
+
+function normalizePathname (pathname: string): string {
+  if (/\/$/.test(pathname)) {
+    pathname = path.join(pathname, 'index.html')
+  }
+  return pathname
+}
+
+function forEachAsync <T>(
+  xs: T[],
+  f: (x: T, next: (isEnd: ?boolean) => void) => void,
+  done: () => void
+): void {
+  if (xs.length === 0) return done()
+
+  const [head, ...tail] = xs
+  f(head, isEnd => {
+    if (isEnd) return done()
+    forEachAsync(tail, f, done)
+  })
 }
--- test/index.js
+++ test/index.js
@@ -1,8 +1,40 @@
-import assert from 'power-assert'
-import { world } from '../src/index'
+import fs from 'fs'
+import path from 'path'
+import assert from 'assert'
+import pug from 'pug'
+import { compileMiddleware } from '../src/index'
 
-describe('Entry point', () => {
-  it('exports world', () => {
-    assert(world === 'Hello World!')
+const noop = () => {}
+
+describe('bs-compile-middleware', () => {
+  it('compiles requested resource', done => {
+    const middleware = compileMiddleware({
+      srcDir: path.resolve(__dirname, 'fixtures'),
+      compilers: [
+        {
+          reqExt: 'html',
+          srcExt: 'pug',
+          compile: (src, filename) => {
+            return pug.render(src, {
+              filename,
+              pretty: true
+            }).trim()
+          }
+        }
+      ]
+    })
+
+    middleware({
+      url: 'http://localhost:3000/'
+    }, {
+      end: dest => {
+        assert.equal(dest, readExpects('index.html'))
+        done()
+      }
+    }, noop)
   })
 })
+
+function readExpects (filename) {
+  return fs.readFileSync(path.resolve(__dirname, 'expects', filename), 'utf8').trim()
+}
